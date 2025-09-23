d06af763ffff6a2bea51c9d2a079c58a529a3b54
Abdullahi Abbas Ribadu <153870577+Abdullahi03-dev@users.noreply.github.com>
2025-09-23 01:15:03 -0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ origins = [
     "http://localhost:5173",  
     "http://localhost:3000",  
     "http://127.0.0.1:5173",
-    "http://127.0.0.1:8000"
+    "http://127.0.0.1:8000",
     "https://socialhub-backend-se80.onrender.com"
 ]
 app.add_middleware(
