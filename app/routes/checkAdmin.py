2a50801a0483994a1f1666e6c908bf9487b2286e
Abdullahi Abbas Ribadu <abdullahiabbasribadu@gmail.com>
2025-09-26 07:33:26 -0700

    fix stuffs

--- app/routes/checkAdmin.py
+++ app/routes/checkAdmin.py
@@ -7,9 +7,9 @@ from .. import models,  database
 
 router=APIRouter(prefix='/checkAdmin')
 
-@router.get('/{userid}')
-def check_Admin(userid:int,db:Session=Depends(database.get_db)):
-    user=db.query(models.User).filter(models.User.id==userid).first()
+@router.get('/{savedEmail}')
+def check_Admin(savedEmail:str,db:Session=Depends(database.get_db)):
+    user=db.query(models.User).filter(models.User.email==savedEmail).first()
     if not user:
         raise HTTPException(status_code=404,detail='USER NOT FOUND')
     return {'is_admin':user.role.lower()=='admin'}
