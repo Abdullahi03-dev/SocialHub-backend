2f130afd6dc96e2a5f44b63470cd18c5e7c56f49
Abdullahi Abbas Ribadu <abdullahiabbasribadu@gmail.com>
2025-09-29 02:00:34 -0700

    added new frontend url

--- app/main.py
+++ app/main.py
@@ -12,7 +12,8 @@ origins = [
     "http://localhost:3000",
     "http://127.0.0.1:5173",
     "http://127.0.0.1:8000",
-    "https://socialhub-backend-se80.onrender.com"
+    "https://socialhub-backend-se80.onrender.com",
+    "https://social-hub-frontend-kappa.vercel.app"
 ]
 
 app.add_middleware(
